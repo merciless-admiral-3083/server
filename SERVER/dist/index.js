e949333622473358a27f3604e0c26cb29e66edc5
Jaspreet Singh Nahal <jaspreetnahal100@gmail.com>
2025-03-26 16:08:45 +0530

    b

---
+++ SERVER/dist/index.js
@@ -0,0 +1,84 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const routes_1 = require("./routes");
+const vite_1 = require("./vite");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.use((req, res, next) => {
+    const start = Date.now();
+    const path = req.path;
+    let capturedJsonResponse = undefined;
+    const originalResJson = res.json;
+    res.json = function (bodyJson, ...args) {
+        capturedJsonResponse = bodyJson;
+        return originalResJson.apply(res, [bodyJson, ...args]);
+    };
+    res.on("finish", () => {
+        const duration = Date.now() - start;
+        if (path.startsWith("/api")) {
+            let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
+            if (capturedJsonResponse) {
+                logLine += ` :: ${JSON.stringify(capturedJsonResponse)}`;
+            }
+            if (logLine.length > 80) {
+                logLine = logLine.slice(0, 79) + "…";
+            }
+            (0, vite_1.log)(logLine);
+        }
+    });
+    next();
+});
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const server = yield (0, routes_1.registerRoutes)(app);
+    app.use((err, _req, res, _next) => {
+        const status = err.status || err.statusCode || 500;
+        const message = err.message || "Internal Server Error";
+        res.status(status).json({ message });
+        throw err;
+    });
+    // importantly only setup vite in development and after
+    // setting up all the other routes so the catch-all route
+    // doesn't interfere with the other routes
+    if (app.get("env") === "development") {
+        yield (0, vite_1.setupVite)(app, server);
+    }
+    else {
+        (0, vite_1.serveStatic)(app);
+    }
+    // ALWAYS serve the app on port 5000
+    // this serves both the API and the client.
+    // It is the only port that is not firewalled.
+    const port = process.env.PORT || 5000;
+    server.listen(port, "0.0.0.0", () => {
+        (0, vite_1.log)(`serving on port ${port}`);
+    }).on('error', (err) => {
+        if (err.code === 'EADDRINUSE' || err.code === 'ENOTSUP') {
+            // Try a different port if the default one fails
+            server.listen(0, "0.0.0.0", () => {
+                const addr = server.address();
+                if (addr && typeof addr === 'object') {
+                    (0, vite_1.log)(`serving on port ${addr.port}`);
+                }
+            });
+        }
+        else {
+            console.error('Server error:', err);
+            process.exit(1);
+        }
+    });
+}))();
+//# sourceMappingURL=index.js.map
